057eb3601cefb05fdd3bdca725c345b70c58e847
Pavan-Satyam <satyampavan3@gmail.com>
2023-10-11 16:15:07 -0700

    Project_update

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// index.js
+const root = document.getElementById("root");
+
+const myHTML = `
+  <div>
+    <h1>Data Mining Project
+    </h1>
+    <img src="https://colab.research.google.com/drive/1C2EwB8jqdbnCV7Ll1JJDseukO7bhlvYf?usp=sharing" alt="Visualization">
+    </div>
+`;
+
+root.innerHTML = myHTML;
